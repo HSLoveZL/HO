36a361565bd9f0ddbba1019a0c1f4ca59269900b
Horton <wcbieyuan@gmail.com>
2016-06-21 20:38:17 +0800

    add blog

--- config.py
+++ config.py
@@ -20,6 +20,7 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = '857018659@qq.com'
     FLASKY_ADMIN = 'wcbieyuan@gmail.com'
+    FLASKY_POSTS_PER_PAGE = 20
 
     @staticmethod
     def init_app(app):
