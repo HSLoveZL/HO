d12f6c95186bd3596f75c19b21dbf72c2c312911
Horton <wcbieyuan@gmail.com>
2016-06-21 21:31:34 +0800

    add comment

--- config.py
+++ config.py
@@ -22,6 +22,7 @@ class Config:
     FLASKY_ADMIN = 'wcbieyuan@gmail.com'
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
+    FLASKY_COMMENTS_PER_PAGE = 30
 
     @staticmethod
     def init_app(app):
